c000d0317ede7ce8bb3301f25ed187416be21826
Austin Jones` <austinhjones3@gmail.com>
2021-09-06 15:58:46 -0500

    change all schema to .graphql extensions and rework schema/index to serve the schema to app indexz

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { ApolloServer } from "apollo-server";
-import typeDefs from "./schema/index.js";
+import db from "./db.js";
+import {
+  checkIfUserExists,
+  checkIfPostExists,
+  findIndexOfItem,
+} from "./middleware.js";
 import resolvers from "./resolvers/index.js";
+import typeDefs from "./schema/index.js";
+import uuidv1 from "uuid/v1.js";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { db, checkIfUserExists, checkIfPostExists, findIndexOfItem, uuidv1 },
+});
 server.listen({ port: 8080 }).then(({ url }) => {
   console.log(`Server ready at ${url || "http://localhost:4000/"}`);
 });
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,25 +1,8 @@
-import { gql } from "apollo-server";
-import userSchema from "./userSchema.js";
-import postSchema from "./postSchema.js";
-import commentSchema from "./commentSchema.js";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 
-const queryAndMutation = gql`
-  type Query {
-    users(query: String): [User!]!
-    posts(query: String): [Post!]!
-    comments(query: String): [Comment!]!
-    me: User!
-    post: Post!
-  }
-
-  type Mutation {
-    createUser(data: CreateUserInput): User!
-    deleteUser(id: ID!): User!
-    createPost(data: CreatePostInput): Post!
-    deletePost(id: ID!): Post!
-    createComment(data: CreateCommentInput): Comment!
-    deleteComment(id: ID!): Comment!
-  }
-`;
-
-export default [queryAndMutation, userSchema, postSchema, commentSchema];
+const getSchema = async () =>
+  await loadSchemaSync("src/schema/*.graphql", {
+    loaders: [new GraphQLFileLoader()],
+  });
+export default await getSchema();
