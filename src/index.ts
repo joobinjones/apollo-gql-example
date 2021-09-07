6d5bcbf25e80e30e5fa2cec87ed427691e99de92
Austin Jones` <austinhjones3@gmail.com>
2021-09-07 12:29:46 -0500

    use correct uuid version

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import { ApolloServer } from "apollo-server";
 import db from "./db";
 import { checkIfUserExists, checkIfPostExists, findIndexOfItem } from "./middleware";
 import schema from "./schema";
-import uuidv1 from "uuid/index";
+import uuid from "uuid/index";
 
 const server = new ApolloServer({
   schema,
-  context: { db, checkIfUserExists, checkIfPostExists, findIndexOfItem, uuidv1 },
+  context: {
+    db,
+    checkIfUserExists,
+    checkIfPostExists,
+    findIndexOfItem,
+    uuidv1: uuid.v1,
+  },
 });
 server.listen({ port: 8080 }).then(({ url }) => {
   console.log(`Server ready at ${url || "http://localhost:4000/"}`);
